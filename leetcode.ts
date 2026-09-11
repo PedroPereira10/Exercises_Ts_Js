50506993eae70ffb3a597c880f24d17343e9e51e
Pedro Pereira <wintechnologie830@gmail.com>
2026-09-11 15:27:24 -0400

    More leetcode added, it's gonna be one each day

--- leetcode.ts
+++ leetcode.ts
@@ -1,16 +1,32 @@
-function removeDuplicates(nums: number[]): number {
-  if (nums.length === 0) {
+// function removeDuplicates(nums: number[]): number {
+//   if (nums.length === 0) {
+//     return 0;
+//   }
+
+//   let k = 1;
+
+//   for (let i = 1; i < nums.length; i++) {
+//     if (nums[i] !== nums[i - 1]) {
+//       nums[k] = nums[i];
+//       k++;
+//     }
+//   }
+
+//   return k;
+// }
+
+function reverse(x: number): number {
+  const INT_MIN = -(2 ** 31);
+  const INT_MAX = 2 ** 31 - 1;
+
+  const reversedStr = Math.abs(x).toString().split("").reverse().join("");
+  const reversedNum = parseInt(reversedStr) * Math.sign(x);
+
+  if (reversedNum < INT_MIN || reversedNum > INT_MAX) {
     return 0;
   }
 
-  let k = 1;
-
-  for (let i = 1; i < nums.length; i++) {
-    if (nums[i] !== nums[i - 1]) {
-      nums[k] = nums[i];
-      k++;
-    }
-  }
-
-  return k;
+  return reversedNum;
 }
+
+console.log(reverse(345));
